ea4a1e996569231f43ef0ec92a46559363f7fbfb
catmania-admin <melanie.willbold@gmail.com>
2025-01-25 14:08:31 +0100

    updated guest enum

---
+++ lib/domain/enums/guest_name.dart
@@ -0,0 +1,12 @@
+enum GuestName {
+  gatow(name: "Dennis Gatow"),
+  bloom(name: "Felix Bloom"),
+  gruen(name: "Tom Grün"),
+  porz(name: "Klara Porz"),
+  roth(name: "Gloria Roth"),
+  weiss(name: "Diana Weiss");
+
+  final String name;
+
+  const GuestName({required this.name});
+}
--- lib/domain/enums/guest_names.dart
+++
@@ -1,8 +0,0 @@
-enum GuestNames {
-  Dennis_Gatow,
-  Felix_Bloom,
-  Tom_Gruen,
-  Klara_Porz,
-  Gloria_Roth,
-  Diana_Weiss,
-}
