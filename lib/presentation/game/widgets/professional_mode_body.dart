d152221564c300fc3aa8fa9983cf101528ca4ac0
rosixmurmi <maggehdx@t-online.de>
2025-02-02 14:28:01 +0100

    added professional body

---
+++ lib/presentation/game/widgets/professional_mode_body.dart
@@ -0,0 +1,27 @@
+import 'package:cluedo_neu/application/gameBloc/game_bloc.dart';
+import 'package:flutter/material.dart';
+
+class ProfessionalModeBody extends StatelessWidget {
+  final GameState gameState;
+  const ProfessionalModeBody({super.key, required this.gameState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: Colors.amber,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  
+}
